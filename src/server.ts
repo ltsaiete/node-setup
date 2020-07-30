ac14e426c9f0c6e7820a6c4b186d93411951e55b
Luis Saiete <luistsaiete@gmail.com>
2020-07-30 17:39:06 +0200

    Adding some eslint rules

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
+import '@controllers/UsersController';
+
 const app = express();
 
-app.get('/', (request, response) => {
-	return response.json({message: 'Hello universe'})
-});
+app.get('/', (request, response) => response.json({ message: 'Hello universe' }));
 
 app.listen(3333);
